06a97d43874762c7dac1f721f6d6abd72ebaf73b
^-^ <bogapova.alia@gmail.com>
2023-06-16 23:56:50 +0300

    commit message

--- main.py
+++ main.py
@@ -22,12 +22,14 @@ class Main(QMainWindow, Ui_MainWindow):
         Class Constructor.
         """
         super().__init__()
+        self.buttons = None
+        self.sb = QStatusBar(self)
         self.eq_btn = QPushButton(self)
         self.plus_btn = QPushButton(self)
         self.set_window()
         self.set_swimming_buttons()
 
-    def set_window(self):  # инициализация
+    def set_window(self):  
         """
         Method sets the window parameters such as window size, background color, and widgets.
         :return: None
@@ -43,7 +45,11 @@ class Main(QMainWindow, Ui_MainWindow):
         self.connect_to_db()
         self.display_widgets_buttons()
 
-    def set_swimming_buttons(self):  # метод для кнопок для перехода в два побочных окна
+    def set_swimming_buttons(self): 
+        """
+        Method for going to new_media window and media_by_categories window
+        :return: None
+        """
         self.plus_btn.clicked.connect(self.open_window_to_add_media_content)
         self.plus_btn.setStyleSheet("QPushButton{\n"
                                     "    background-color: rgb(201, 164, 255);\n"
@@ -68,30 +74,37 @@ class Main(QMainWindow, Ui_MainWindow):
         self.eq_btn.setFixedSize(40, 40)
         self.container()
 
-    def container(self):  # метод для котейнера этих кнопок в статусбаре
-        self.sb = QStatusBar(self)
+    def container(self):
+        """
+        Method containing plus_btn and eq_btn
+        :return: None
+        """
         self.sb.setStyleSheet('background-color: rgba(0, 0, 0,0);')
         self.sb.move(10, 10)
         self.sb.addWidget(self.plus_btn)
         self.sb.addWidget(self.eq_btn)
         self.sb.setFixedSize(100, 40)
 
-    def display_widgets_buttons(self):  # метод для отображения картинок, кнопок и информации
+    def display_widgets_buttons(self):
+        """
+        Method for displaying pictures, buttons and information
+        :return: None
+        """
         scr = QScrollArea(self)
         scr.setWidgetResizable(True)
         pnl = QWidget(self)
         layout = QGridLayout(self)
-        self.btns = {i: QPushButton(self) for i in range(len(self.res))}
-        self.redact_btns = {i: QPushButton(self) for i in range(len(self.res))}
-        self.remove_btns = {i: QPushButton(self) for i in range(len(self.res))}
+        self.buttons = {i: QPushButton(self) for i in range(len(self.res))}
+        self.redact_buttons = {i: QPushButton(self) for i in range(len(self.res))}
+        self.remove_buttons = {i: QPushButton(self) for i in range(len(self.res))}
         self.titles = []
 
         self.res.reverse()
         self.puti.reverse()
         for i in range(len(self.res)):
-            pic_btn = self.btns[i]
-            redact_btn = self.redact_btns[i]
-            remove_btn = self.remove_btns[i]
+            pic_btn = self.buttons[i]
+            redact_btn = self.redact_buttons[i]
+            remove_btn = self.remove_buttons[i]
             redact_btn.setStyleSheet("QPushButton{\n"
                                      "    background-color: rgb(201, 164, 255);\n"
                                      "    border-radius: 10px;\n"
@@ -129,9 +142,9 @@ class Main(QMainWindow, Ui_MainWindow):
             remove_btn.clicked.connect(self.remove_from_list)
             self.text = QPlainTextEdit(self)
             self.titles.append(a[0])
-            pola = ['title', 'status', 'type', 'progress', 'rating', 'review']
+            fields = ['title', 'status', 'type', 'progress', 'rating', 'review']
             for k in range(6):
-                self.text.appendPlainText(f'{pola[k]}: {a[k]}')
+                self.text.appendPlainText(f'{fields[k]}: {a[k]}')
             self.text.setReadOnly(True)
             self.text.setStyleSheet('background-color: rgb(241, 231, 255);')
             layout.addWidget(pic_btn, i, 3)
@@ -148,49 +161,57 @@ class Main(QMainWindow, Ui_MainWindow):
         self.setCentralWidget(scr)
 
     def remove_from_list(self):  # метод для удаления из списка
-        for k in self.remove_btns.keys():
-            if self.remove_btns[k] == self.sender():
+        for k in self.remove_buttons.keys():
+            if self.remove_buttons[k] == self.sender():
                 nam = self.titles[k]
         cur = self.connection.cursor()
-        cur.execute("""DELETE from titles where название = ?""", (nam,))
-        cur.execute("""DELETE from pictures where название = ?""", (nam,))
+        cur.execute("""DELETE from titles where title = ?""", (nam,))
+        cur.execute("""DELETE from pictures where title = ?""", (nam,))
         self.connection.commit()
         self.connect_to_db()
         self.open_main_window()
 
-    def edit_list(self):  # метод для редактирования в списке
+    def edit_list(self):
+        """
+        Method for list redaction
+        :return: None
+        """
         uic.loadUi('a2.ui', self)
-        for k in self.redact_btns.keys():
-            if self.redact_btns[k] == self.sender():
-                self.reductObj = self.titles[k]
+        for k in self.redact_buttons.keys():
+            if self.redact_buttons[k] == self.sender():
+                self.redact_obj = self.titles[k]
                 self.back.hide()
                 self.save_editing()
 
-    def save_editing(self):  # метод для сохранения отредактированного
-        self.vvodtext.setText(self.reductObj)
-        btnsarr = [self.r1, self.r2, self.r3, self.r4, self.r5, self.r6, self.r7, self.r8, self.r9]
+    def save_editing(self):
+        """
+        Method for saving changes
+        :return: None
+        """
+        self.vvodtext.setText(self.redact_obj)
+        btn_arr = [self.r1, self.r2, self.r3, self.r4, self.r5, self.r6, self.r7, self.r8, self.r9]
         for el in self.res:
-            if el[0] == self.reductObj:
+            if el[0] == self.redact_obj:
                 self.ocenka.setMaximum(10)
                 self.ocenka.setValue(el[4])
                 self.statusbox.setCurrentText(el[1])
                 self.otzyv.setPlainText(el[5])
-                for but in btnsarr:
+                for but in btn_arr:
                     if but.text() == el[2]:
                         but.setChecked(True)
         for el in self.puti:
-            if el[0] == self.reductObj:
+            if el[0] == self.redact_obj:
                 self.putreduct = el[1]
         cur = self.connection.cursor()
-        cur.execute("""DELETE from titles where название = ?""", (self.reductObj,))
-        cur.execute("""DELETE from pictures where название = ?""", (self.reductObj,))
+        cur.execute("""DELETE from titles where название = ?""", (self.redact_obj,))
+        cur.execute("""DELETE from pictures where название = ?""", (self.redact_obj,))
         self.connection.commit()
         self.connect_to_db()
         self.btnadd.clicked.connect(self.save_info)
 
     def select_picture(self):  # метод для выбора картинки при нажатии на имгбаттон
-        for k in self.btns.keys():
-            if self.btns[k] == self.sender():
+        for k in self.buttons.keys():
+            if self.buttons[k] == self.sender():
                 fname = QFileDialog.getOpenFileName(
                     self, 'Select a picture', '',
                     'Picture (*.jpg);;Picture (*.jpg);;All files (*)')[0]
@@ -199,7 +220,7 @@ class Main(QMainWindow, Ui_MainWindow):
                                 SET путь = ?
                                 WHERE title = ?""", (fname, self.titles[k]))
                 self.connection.commit()
-                self.btns[k].setStyleSheet(f'background-image : url({fname});')
+                self.buttons[k].setStyleSheet(f'background-image : url({fname});')
                 self.connect_to_db()
                 self.open_main_window()
 
