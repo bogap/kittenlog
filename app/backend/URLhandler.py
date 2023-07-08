94503a22ba4243b5d0fa5a3009010ed15bc2c7ee
MilyaushaShamsutdinova <m.shamsutdinova@innopolis.university>
2023-07-08 12:51:55 +0300

    docs: code documentation

--- app/backend/URLhandler.py
+++ app/backend/URLhandler.py
@@ -3,38 +3,51 @@ from urllib import request
 
 
 class URLView(Qt.QWidget):
+    """
+    URLView class represents a widget for displaying an image from a given URL.
+
+    Attributes:
+        urlEdit (QLineEdit): A QLineEdit widget to display the URL of the image.
+        imageLabel (QLabel): A QLabel widget to display the image.
+    """
 
     def __init__(self, url, parent=None):
+        """
+        Initializes an instance of the URLView class.
+
+        Args:
+            url (str): The URL of the image to display.
+            parent (QWidget): The parent widget. Default is None.
+        """
 
         super().__init__(parent)
-        # Call the constructor of the parent class (QWidget)
-
         layout = Qt.QVBoxLayout(self)
 
         self.urlEdit = Qt.QLineEdit()
         self.urlEdit.setText(url)
-        # Set a URL in the QLineEdit widget
 
         self.imageLabel = Qt.QLabel()
         self.imageLabel.setScaledContents(True)
         layout.addWidget(self.imageLabel)
-        # Set the place to image
+
+        self._load_image_from_url()
+
+    def _load_image_from_url(self):
+        """
+        Loads the image data from the specified URL and sets it in the imageLabel widget.
+        """
 
         data = request.urlopen(self.urlEdit.text()).read()
         pixmap = Qt.QPixmap()
         pixmap.loadFromData(data)
         self.imageLabel.setPixmap(pixmap)
-        # Retrieve the image data by opening the URL and load the image
+
 
 if __name__ == '__main__':
     app = Qt.QApplication([])
-    # Create an instance of QApplication
 
     w = URLView("https://moodle.innopolis.university/pluginfile.php/1/theme_academi/logo/1686640507/logo.png")
-    # Create an instance of URLView
 
     w.show()
-    # Display the URLView widget
 
     app.exec()
-    # Start the event loop of the application
